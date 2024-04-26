a2855419f73eb8b6e35690de237f28d00c550de9
Ankit Kumar <105992425+ankitpurbey@users.noreply.github.com>
2024-04-26 13:01:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = express();
 const mongoose = require("mongoose")
 
 main()
-.then
+.then(()=>{
+    console.log("Connected to DB");
+})
+.catch((err)=>{
+    console.log(err);
+});
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/WANDER_LUST');
   }
